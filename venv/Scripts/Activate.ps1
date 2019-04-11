dda224dfedff402722def49ac9583f7879686f4f
William Fuzitani <williamfuzitani@gmail.com>
2019-04-11 15:36:40 -0300

    Adicionado obstaculos e aumento na dimensao do mapa

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Administrador\PycharmProjects\PuzzleProject\venv"
+$env:VIRTUAL_ENV="D:\Downloads\game-python-ice-sliding-puzzle\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Administrador\PycharmProjects\PuzzleProject\venv"
+set "VIRTUAL_ENV=D:\Downloads\game-python-ice-sliding-puzzle\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
